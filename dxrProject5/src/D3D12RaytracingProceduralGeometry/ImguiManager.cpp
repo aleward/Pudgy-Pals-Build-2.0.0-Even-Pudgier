273043c3825d8b75b55d35b3ed0ff01ecbda4d08
BobMowzie <jnad325@gmail.com>
2019-12-03 13:48:39 -0500

    imgui compiles!

---
+++ dxrProject5/src/D3D12RaytracingProceduralGeometry/ImguiManager.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "ImguiManager.h"
+
+
+ImguiManager::ImguiManager()
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui::StyleColorsDark();
+}
+
+
+ImguiManager::~ImguiManager()
+{
+	ImGui::DestroyContext();
+}
